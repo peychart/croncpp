2d9880180e3e00231c581c6a49e19493571d9bf5
rickky <peychart@users.noreply.github.com>
2022-07-03 17:31:16 -1000

    Update cron.cpp

--- cron.cpp
+++ cron.cpp
@@ -130,7 +130,7 @@ namespace cronTab{
     return date.assign( &timeinfo );
   }
 
-  time_t  cron::dateAround( const std::tm& timeinfo, bool next ){
+  time_t  cron::dateAround( const std::tm& timeinfo, bool next ){bool match(true);
     if( convError() ) return time_t(-1);
     std::tm result( timeinfo );
     for( byte nfield(0); existingField(nfield); nfield++ ){
@@ -146,7 +146,7 @@ namespace cronTab{
       }else while( true ){
         if( isSet( nfield, i ) )
           break;
-        delta += (next ?1 :-1);
+        match |= !( delta += (next ?1 :-1) );
         if( ( (i += (next ?1 :-1)) >= ( monthSize ?monthSize :field_size[nfield] ) ) )
           i = ( next ?0 :( ( monthSize ?monthSize :field_size[nfield] ) - 1 ) );
         if( i == j )  return time_t(-1);
@@ -155,7 +155,9 @@ namespace cronTab{
       if( delta ){
         *tminfo[(nfield == field_name::day_of_week) ?field_name::day_of_month :nfield] += delta;
         while( existingField(--nfield ) )
-          *tminfo[nfield] = ( next ?( (nfield==field_name::day_of_month) ?1 :0 ) :(isSet(nfield) ?0 :( monthSize ?monthSize : field_size[nfield]-1)) );
+          *tminfo[nfield] = ( next ?( (nfield==field_name::day_of_month) ?1 :0 ) :( monthSize ?monthSize : field_size[nfield]-1) );
+      }else if( nfield == field_name::year && match ){
+        result.tm_sec=(next ?60 :0); match=false; nfield=-1;
     } }
     return mktime( &result );
   }
